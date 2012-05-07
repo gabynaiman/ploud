be8f2ce41cbaeec0ae64aeeccdd4014deaa02b04
Gabriel Namiman <gabynaiman@gmail.com>
2012-05-07 10:18:12 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ploud::Application.config.secret_token = '686f1d575bf991923f04d7c9e07052fe83d0d600fe6b7e1a2644cfeaeea67b14b78be6474041edf738a284acd201e887e85a679269ec824091de915daa4ecd1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ploud::Application.config.session_store :cookie_store, key: '_ploud_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ploud::Application.config.session_store :active_record_store
